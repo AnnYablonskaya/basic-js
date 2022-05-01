772cc175a3ebb1a9dce6d1f70f70d88e08f57349
AnnYablonskaya <92150346+AnnYablonskaya@users.noreply.github.com>
2022-05-01 15:55:35 +0300

    update

--- src/what-season.js
+++ src/what-season.js
@@ -13,6 +13,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function getSeason(date) {
   let month = date.getMonth();
+  if (Object.prototype.toString.call(date) !== "[object Date]"  
+  || isNaN(date)) {
+throw new Error('Invalid date!') }
+
   if (month === 0 || month === 1 || month === 11){
     return "winter";
   }else if(month === 2 || month === 3 || month === 4){
@@ -21,8 +25,13 @@ function getSeason(date) {
     return "summer";
   } else if(month === 8 || month === 9 || month === 10){
     return "autumn";
-  } else if(month === undefined){
+  } else if(!date){
     return 'Unable to determine the time of year!';
-  }else throw new Error('Invalid date!');
-
+  }
 }
+ 
+  
+module.exports = {
+  getSeason
+};
+
