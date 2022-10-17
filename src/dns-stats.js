7c6b499903fb44a74bdf9a034766fa23889050ce
AnnYablonskaya <yaanna1910@gmail.com>
2022-10-17 13:35:22 +0300

    dns

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,10 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+
+  let newDNS = {};
+  
+  for (let i = 0; i < domains.length; i++) {
+    let array = domains[i].split('.').reverse();
+    let  item='';
+    for (let j = 0; j < array.length; j++) {
+      item = item + '.' + array[j];
+      newDNS.hasOwnProperty(item) ? newDNS[item]++ : newDNS[item] = 1;
+    }
+  }
+  return newDNS;
 }
+
 
 module.exports = {
   getDNSStats
